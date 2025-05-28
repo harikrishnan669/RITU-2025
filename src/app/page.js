7c70185f1e9d565a82dc8046c579f72d8abff172
fbn776 <fbn776@gmail.com>
2025-05-28 14:49:57 +0530

    chore: ritu 25

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
 
     return <main className="overflow-y-auto size-full">
         <div className="w-full h-full flex items-center justify-center">
-            <MorphingText texts={["റിതു", "RITU", "ரிது", "ऋतु", "ಋತು"]} className="text-white"/>
+            <MorphingText texts={["ഋതു 25", "RITU 25", "ரிது 25", "ऋतु 25", "ಋತು 25"]} className="text-white"/>
         </div>
     </main>
 }
