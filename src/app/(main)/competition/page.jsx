2c1bd87ec7b4cebd29d62297651edf9bfd27706a
fbn776 <fbn776@gmail.com>
2025-06-01 23:38:22 +0530

    feat: pages

---
+++ src/app/(main)/competition/page.jsx
@@ -0,0 +1,7 @@
+
+
+export default function TEst() {
+    return <div>
+        Hahah
+    </div>
+}
--- src/components/home/cultural-events.jsx
+++ src/components/home/cultural-events.jsx
@@ -1,6 +1,5 @@
 import Header from "@/components/header";
 
-
 export default function CulturalEvents() {
     return <section className="container-cont">
         <Header title="Culturals"/>
