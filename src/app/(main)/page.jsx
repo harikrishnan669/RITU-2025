4584362692dd5b6d4001f2e895f743af5c36fa21
fbn776 <fbn776@gmail.com>
2025-06-03 23:55:10 +0530

    fix: vercel deployment issue

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import LandingScreen from "@/components/home/landing-screen";
-import MeetOurSponsors from "@/components/home/meet-our-sponsors";
 import MeetOurSponsorsNew from "@/components/home/meet-our-sponsors-new";
 import AboutUs from "@/components/home/about-us";
 import EventsListing from "@/components/home/events-listing";
@@ -9,7 +8,7 @@ import CulturalEvents from "@/components/home/cultural-events";
 import Gallery from "@/components/home/gallery";
 
 export default function Home() {
-    return <main className="overflow-y-auto size-full h-[4000px]">
+    return <main className="overflow-y-auto size-full">
         <LandingScreen/>
         <AboutUs/>
         <EventsListing/>
