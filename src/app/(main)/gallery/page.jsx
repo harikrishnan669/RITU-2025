5ccc0578e9e6dc006596d4224786b6dd6b2f8383
abhiram <abhiramashok1062004@gmail.com>
2025-06-05 19:37:28 +0530

    abhi/Gallery Listing Page

--- src/app/(main)/gallery/page.jsx
+++ src/app/(main)/gallery/page.jsx
@@ -2,7 +2,7 @@ import Header from "@/components/header";
 import BlurFade from "@/components/magicui/blurr-fade";
 
 export default function Test() {
-    const images = [...Array(9)].map((_, i) => {
+    const images = [...Array(20)].map((_, i) => {
         const isLandscape = i % 2 === 0;
         const width = isLandscape ? 800 : 600;
         const height = isLandscape ? 600 : 800;
@@ -12,7 +12,7 @@ export default function Test() {
     return <div className="pt-20">
         <Header title="Gallery"/>
         <section className="px-22 mb-10 py-2 max-sm:px-6">
-            <div className="columns-2 gap-4 sm:columns-3">
+            <div className="columns-2 gap-4 sm:columns-3 md:columns-4">
                 {images.map((imageUrl, index) => (
                         <BlurFade key={imageUrl} delay={0.25 + index * 0.05} inView>
                             <img
