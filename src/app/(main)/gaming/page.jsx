5dcefea1319ad6a5e49e429ffb0fe58ab2904f18
farza <farzanrichu610@gmail.com>
2025-06-07 17:59:32 +0530

    added:gaming-background

--- src/app/(main)/gaming/page.jsx
+++ src/app/(main)/gaming/page.jsx
@@ -1,7 +1,7 @@
 
 
-export default function TEst() {
-    return <div>
-        Hahah
+export default function Gaming() {
+    return <div className="flex flex-col justify-center items-center h-screen">
+       starting Here
     </div>
 }
