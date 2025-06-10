1b6bd4c0e6106e03b3f3d1bf7c13723eacf1b6af
Hari <rharikrishnan2020@gmail.com>
2025-06-10 20:12:01 +0530

    Card changed to component

--- src/app/(main)/competition/page.jsx
+++ src/app/(main)/competition/page.jsx
@@ -1,43 +1,8 @@
 "use client";
 import React, {useRef, useState} from "react";
-import {Calendar, Funnel, MapPin} from "lucide-react"
+import {Funnel} from "lucide-react";
+import EventCard from "@/components/cards";
 
-function EventCard({event}) {
-    return (
-        <div
-            className="bg-white/10 border border-white/20 rounded-xl shadow-lg overflow-hidden max-w-sm w-full flex flex-col">
-            <div className="relative">
-                <img
-                    src={event.image}
-                    alt={event.title}
-                    className="w-full h-40 object-cover"
-                />
-                {event.badge && (
-                    <span
-                        className="absolute top-2 right-2 bg-purple-600 text-white text-xs font-semibold px-3 py-1 rounded-full">
-            {event.badge}
-          </span>
-                )}
-            </div>
-            <div className="p-4 flex flex-col flex-grow">
-                <div className="flex items-center text-sm text-white-500 mb-1">
-                    <span className="mr-2"><Calendar className="size-5"/></span> {event.date}
-                </div>
-                <h2 className="text-lg font-semibold text-white-800 mb-1">
-                    {event.title}
-                </h2>
-                <p className="text-white-600 mb-3 flex-grow text-sm">{event.description}</p>
-                <div className="flex items-center text-sm text-white-500 mb-3">
-                    <span className="mr-2"><MapPin className="size-5"/></span> {event.location}
-                </div>
-                <button
-                    className="bg-purple-900 text-white py-2 px-4 rounded-md hover:bg-purple-800 transition self-end">
-                    Book Now
-                </button>
-            </div>
-        </div>
-    );
-}
 const events = [
     {
         image: "https://images.pexels.com/photos/2747449/pexels-photo-2747449.jpeg?cs=srgb&dl=pexels-wolfgang-1002140-2747449.jpg&fm=jpg",
