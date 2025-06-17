a94f06028e3b21c8a7bdc6446f5433c492780145
fbn776 <fbn776@gmail.com>
2025-06-18 01:12:32 +0530

    feat: logo wall

--- src/components/home/meet-our-sponsors-new.jsx
+++ src/components/home/meet-our-sponsors-new.jsx
@@ -1,86 +1,12 @@
 import Header from "@/components/header";
-import Marquee from "react-fast-marquee";
+import LogoWall from "@/components/logo-wall/logo-wall";
 
 export default function MeetOurSponsorsNew() {
 	return (
 		<>
 			<Header title="Meet Our Partners" />
-			<Marquee speed={50} gradient={false} direction="left" className="">
-				<div className="flex flex-row justify-evenly  items-center w-full h-full">
-					<img
-						src="/sponsors/berger.png"
-						alt="Berger"
-						className=" bg-white px-10 py-3  rounded-xl mx-10 h-32"
-					/>
-					<img
-						src="/sponsors/sbi.png"
-						alt="SBI"
-						className="bg-white px-10 py-3 rounded-xl mx-10 h-32"
-					/>
-					<img
-						src="/sponsors/lic.png"
-						alt="LIC"
-						className="bg-white px-10 py-3 rounded-xl mx-10 h-32"
-					/>
-
-					<img
-						src="/sponsors/berger.png"
-						alt="Berger"
-						className=" bg-white px-10 py-3  rounded-xl mx-10 h-32"
-					/>
-					<img
-						src="/sponsors/sbi.png"
-						alt="SBI"
-						className="bg-white px-10 py-3 rounded-xl mx-10 h-32"
-					/>
-					<img
-						src="/sponsors/lic.png"
-						alt="LIC"
-						className="bg-white px-10 py-3 rounded-xl mx-10 h-32"
-					/>
-				</div>
-			</Marquee>
-
-			<Marquee
-				speed={50}
-				gradient={false}
-				direction="right"
-				className="mt-10 mb-10"
-			>
-				<div className="flex flex-row justify-evenly items-center w-full h-full">
-					<img
-						src="/sponsors/berger.png"
-						alt="Berger"
-						className=" bg-white px-10 py-3  rounded-xl mx-10 h-32"
-					/>
-					<img
-						src="/sponsors/sbi.png"
-						alt="SBI"
-						className="bg-white px-10 py-3 rounded-xl mx-10 h-32"
-					/>
-					<img
-						src="/sponsors/lic.png"
-						alt="LIC"
-						className="bg-white px-10 py-3 rounded-xl mx-10 h-32"
-					/>
-
-					<img
-						src="/sponsors/berger.png"
-						alt="Berger"
-						className=" bg-white px-10 py-3  rounded-xl mx-10 h-32"
-					/>
-					<img
-						src="/sponsors/sbi.png"
-						alt="SBI"
-						className="bg-white px-10 py-3 rounded-xl mx-10 h-32"
-					/>
-					<img
-						src="/sponsors/lic.png"
-						alt="LIC"
-						className="bg-white px-10 py-3 rounded-xl mx-10 h-32"
-					/>
-				</div>
-			</Marquee>
+			<LogoWall/>
+			<br/><br/>
 		</>
 	);
 }
