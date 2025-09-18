4291e84054ff99dcc0110006b563664607e46266
fbn776 <fbn776@gmail.com>
2025-09-19 00:29:25 +0530

    chore: removed additional placeholder poster

--- src/data/proshows.js
+++ src/data/proshows.js
@@ -7,16 +7,7 @@ const PROSHOWS = [
         placeholder: true
     }, {
         img: "/posters/proshows/jobkurian.webp",
-        date: "March 21, 7pm-12pm",
-        location: "College Auditorium",
-        buttonColor: "#08070D",
-        placeholder: true
-    }, {
-        img: "/placeholder/coming-soon-proshow.webp",
-        date: "March 21, 7pm-12pm",
-        location: "College Auditorium",
-        buttonColor: "#08070D",
-        placeholder: true
+
     }, {
         img: "/placeholder/coming-soon-proshow.webp",
         date: "March 21, 7pm-12pm",
