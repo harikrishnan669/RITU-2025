61305cbd67424c92b55ef80bacd3c89c4ebba349
fbn776 <fbn776@gmail.com>
2025-09-19 00:41:18 +0530

    responsive: changes to about section

--- src/components/home/about-us.jsx
+++ src/components/home/about-us.jsx
@@ -20,15 +20,14 @@ export default function AboutUs() {
                 </div>
             </div>
             <div className="mt-16 mb-16 lg:max-w-6xl max-lg:max-w-3xl max-md:max-w-full max-md:px-6 mx-auto
-                text-justify md:tracking-wide leading-[2rem] md:leading-[2.2rem] space-y-6 text-xl max-md:text-base">
-
+                text-justify max-sm:text-left md:tracking-wide leading-[2rem] md:leading-[2.2rem] space-y-6 text-xl max-md:text-base">
                 <p>
-                    <strong className="text-white-600 font-secondary text-4xl">Ritu</strong> ,the vibrant techno-cultural fest of Rajiv Gandhi
+                    <strong className="text-white-600 font-secondary text-4xl">Ritu</strong>, the vibrant techno-cultural fest of Rajiv Gandhi
                     Institute of Technology,
                     Kottayam, is all set to take place on
                 </p>
                 <div className="flex flex-col items-center mt-6 mb-8 text-center">
-                    <span className="text-xl md:text-8xl font-poppins font-semibold mb-2 font-secondary">October</span>
+                    <span className="text-6xl md:text-8xl font-poppins font-semibold mb-2 font-secondary">October</span>
                     <div className="flex gap-4 text-3xl md:text-5xl font-poppins font-bold tracking-wide">
                         <span className="text-[#cd222d]">16</span>
                         <span className="text-[#1d96d5]">17</span>
@@ -37,37 +36,36 @@ export default function AboutUs() {
                     </div>
                 </div>
 
-                <div className="space-y-6 leading-relaxed text-lg md:text-xl font-inter">
-
+                <div className="space-y-6 leading-relaxed text-lg md:text-xl">
                     <p>
                         This four-day extravaganza will feature a wide range of activities including
-                        <span className="font-semibold font-secondary"> hands-on workshops</span> led by industry
+                        <span className="sm:font-semibold sm:font-secondary"> hands-on workshops</span> led by industry
                         experts,
-                        <span className="font-semibold"> insightful talk sessions</span> from inspiring
+                        <span className="sm:font-semibold"> insightful talk sessions</span> from inspiring
                         speakers,
-                        and <span className="font-semibold text-3xl font-secondary"> thrilling games & competitions</span> that
+                        and <span className="sm:font-semibold sm:text-3xl sm:font-secondary"> thrilling games & competitions</span> that
                         test your skills and
                         team spirit.
                     </p>
 
                     <p>
                         Alongside the technical side, the fest will showcase
-                        <span className="font-semibold font-secondary text-3xl"> vibrant cultural performances</span> that bring
+                        <span className="sm:font-semibold sm:font-secondary sm:text-3xl"> vibrant cultural performances</span> that bring
                         out the artistic flair of our campus —
                         making RITU a true celebration of both <span
-                        className="font-bold font-secondary text-3xl">innovation</span> and
-                        <span className="font-bold font-secondary text-3xl"> creativity</span>.
+                        className="font-bold sm:font-secondary sm:text-3xl">innovation</span> and
+                        <span className="font-bold sm:font-secondary sm:text-3xl"> creativity</span>.
                     </p>
 
-                    <p>
-                        Whether you are a <span className="font-semibold">tech enthusiast</span>, a
-                        <span className="font-semibold"> creative soul</span>, or simply someone who
+                    <p className="max-sm:hidden">
+                        Whether you are a <span className="sm:font-semibold">tech enthusiast</span>, a
+                        <span className="sm:font-semibold"> creative soul</span>, or simply someone who
                         loves being part of an
-                        <span className="font-semibold text-3xl font-secondary"> energetic and youthful crowd</span>,
+                        <span className="sm:font-semibold sm:text-3xl sm:font-secondary"> energetic and youthful crowd</span>,
                         RITU has something for you.
                     </p>
 
-                    <p className="font-poppins font-bold text-3xl md:text-3xl text-center mt-8 font-secondary">
+                    <p className="max-sm:hidden font-bold text-3xl md:text-3xl text-center mt-8 font-secondary">
                         Join us and be part of the electrifying spirit that defines ritu!
                     </p>
                 </div>
