a16b5d7334bba96e2a06f06fbcf7c3dde289d732
fbn776 <fbn776@gmail.com>
2025-10-07 22:17:42 +0530

    feat: add superbike

---
+++ src/data/events/workshop.ts
@@ -0,0 +1,37 @@
+const WORKSHOPS = [
+    {
+        image: '/posters/workshops/mini-makers.webp',
+
+        title: 'MINI MAKERS - 2025',
+        date: '3rd Week September 2025',
+        description: `A day to take technology beyond the classroom!
+
+        Mini Makers opens the door for young students to explore technology in a hands-on, playful way.
+
+    Hands-on activities and showcases let students explore peer creations—making the day interactive and exciting!!
+
+    Let’s make, tinker, and have fun with technology at Mini Makers 2025!
+`,
+        instagram: "https://www.instagram.com/share/p/BAkuismkfs",
+        contact: [{name: 'Gowripriya M J', number: '9061017061'}],
+        location: 'Online',
+        endDate: '2025-09-21T00:00:00+05:30',
+    }, {
+        "image": "/posters/competition/super-bike.webp",
+        "date": "2025-10-18T00:00:00+05:30",
+        "title": "Super Bike",
+        "description": "Get ready to rev up your engineering skills.\nJoin us for an engaging and practical workshop that delves into the world of superbikes — from design principles and performance dynamics to the intricate working of Engine Control Units. This is a unique opportunity to gain hands-on exposure to real-world engineering concepts and applications.\n\nDate: 18 October\nVenue: Rit Kottayam\nRegistration: ritu25.live\n\nRegistration Fee:\n₹899 – Superbike Design\n₹799 – ECU\n₹1300 – Both Sessions\n\nActivity Points: Up to 60 points\n\nFor Queries:\nSabari – 6238908262\n\n#RITU25 #SuperbikeDesign #ECUWorkshop #RITKottayam #EngineeringWorkshop #AutomotiveEngineering #HandsOnLearning",
+        "location": "RIT",
+        "url": "https://www.yepdesk.com/embed/open-register/68e35d9046e0fb00015b0d83/ff6rf6tn90",
+        "contact": [
+            {
+                "name": "Sabari",
+                "number": "6238908262"
+            }
+        ],
+        "endDate": "2025-10-18T00:00:00+05:30",
+        "createdDate": "2025-10-07T16:43:59.139Z"
+    }
+];
+
+export default WORKSHOPS
