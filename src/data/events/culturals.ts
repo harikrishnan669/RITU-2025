2ba0e487cb654e003f108b1cba6786d70de9acee
fbn776 <fbn776@gmail.com>
2025-10-10 00:00:13 +0530

    add new poster

--- src/data/events/culturals.ts
+++ src/data/events/culturals.ts
@@ -1,5 +1,25 @@
 import {IEventData} from "@/types/event";
 
-const CULTURALS: IEventData[] = []
+const CULTURALS: IEventData[] = [
+    {
+        "image": "/posters/culturals/RITU.webp",
+        "date": "2025-10-15T00:00:00+05:30",
+        "title": "Be The Sound Of RITU",
+        "description": "RITU 25 is calling all performers to the Informals Stage!\n\nGet ready to set the vibe and own the spotlight! The Informals Stage at RITU 25 is open for all artists — singers, dancers, musicians, and performers from outside RIT too. Bring your energy, your art, and your rhythm — and make the crowd groove to your beat!\n\nContact:\nPrabhav: 9747150277\nGamirel: 8547538289\n\n#RITU25 #InformalsStage #OpenCall #PerformAtRITU #RITKottayam #CulturalFest #ArtistsUnite #RITU25Live",
+        "location": "RIT",
+        "contact": [
+            {
+                "name": "Prabhav",
+                "number": "9747150277"
+            },
+            {
+                "name": "Gamirel",
+                "number": "8547538289"
+            }
+        ],
+        "endDate": "2025-10-17T00:00:00+05:30",
+        "createdDate": "2025-10-09T18:26:59.526Z"
+    }
+]
 
 export default CULTURALS;
