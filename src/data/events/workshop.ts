8efd308025f4ecbe2417629a08a62d67c5b3d847
fbn776 <fbn776@gmail.com>
2025-10-09 16:07:15 +0530

    poster

--- src/data/events/workshop.ts
+++ src/data/events/workshop.ts
@@ -51,6 +51,118 @@ const WORKSHOPS = [
         "createdDate": "2025-10-09T05:46:06.950Z",
         instagram: "https://www.instagram.com/share/p/BA99VSp155",
         url: "https://www.yepdesk.com/embed/open-register/68e54a6646e0fb0001623a04/beeumnr1ip"
+    },
+    {
+        "image": "/posters/workshops/drone.webp",
+        "date": "2025-10-17T00:00:00+05:30",
+        "title": "Drone Making Bootcamp",
+        "description": "Get ready to take flight into the world of innovation!\nJoin us for an exciting and immersive Drone Making Bootcamp, where you'll learn the fundamentals of drone design, assembly, and flight control.\nFrom aerodynamics and electronics to real-world applications, this workshop offers a complete hands-on experience to bring your own drone to life!\n\nDates: 17 & 18 October\nVenue: Rit Kottayam\nRegistration Fee: ₹1000\n\nActivity Points: Up to 50 points\n\nRegister at: ritu25.live\n\nFor Queries:\nJolsina: 9447784218\n\n#RITU25 #DroneMaking #Bootcamp #InnovationInAction #ElectronicsEngineering #HandsOnLearning #AerialTech #RITKottayam",
+        "location": "RIT",
+        "url": "https://www.yepdesk.com/embed/open-register/68e76e7e46e0fb00016d7ded/i9dd2jg67t",
+        "contact": [
+            {
+                "name": "Jolsina",
+                "number": "9447784218"
+            }
+        ],
+        "endDate": "2025-10-17T00:00:00+05:30",
+        "createdDate": "2025-10-09T08:40:54.451Z"
+    }, {
+        "image": "/posters/workshops/aerospace.webp",
+        "date": "2025-10-19T00:00:00+05:30",
+        "title": "Opportunities In Aerospace Materials",
+        "description": "Get ready to explore the future of aerospace innovation!\nJoin us for a captivating workshop on Opportunities in Aerospace Materials, where you'll discover how advanced materials are shaping the next generation of aircraft and engines.\nGain valuable insights into materials engineering, design challenges, and real-world applications from an industry expert.\n\nSpeaker:\nMr. Azad V\nMaterials Engineer, Rolls-Royce India\n\nDate: 19 October\nVenue: Rit Kottayam\nRegistration Fee: ₹499\n\nActivity Points: Up to 50 points\n\nRegister at: ritu25.live\n\nFor Queries:\nJoseph George: 9747296042\nMidhun Regi: 9656747960\n\n#RITU25 #AerospaceMaterials #RollsRoyce #EngineeringInnovation #MechanicalEngineering #AerospaceTech #RITKottayam #HandsOnLearning",
+        "location": "RIT",
+        "url": "https://www.yepdesk.com/embed/open-register/68e771fd46e0fb00016d897a/1ri6rl9ve3",
+        "contact": [
+            {
+                "name": "Joseph George",
+                "number": "9747296042"
+            },
+            {
+                "name": " Midhun Regi",
+                "number": "9656747960"
+            }
+        ],
+        "endDate": "2025-10-19T00:00:00+05:30",
+        "createdDate": "2025-10-09T08:52:11.822Z"
+    }, {
+        "image": "/posters/workshops/vlsi.webp",
+        "date": "2025-10-19T00:00:00+05:30",
+        "title": "VLSI Workshop",
+        "description": "Dive into the world of semiconductor innovation!\nJoin us for an exciting VLSI Workshop and discover how complex integrated circuits are designed and verified in the modern electronics industry.\nLearn about the fundamentals of chip design, verification, and real-world applications from an industry expert.\n\nSpeaker:\nIrene Rose Mathew\nDesign Verification Engineer, Samsung Semiconductor R&D, Bangalore\n\nDate: 19 October\nVenue: Rit Kottayam\nRegistration Fee: ₹150\n\nActivity Points: Up to 20 points\n\nRegister at: ritu25.live\n\nFor Queries:\nRishan: 9539096681\nVismaya: 6235807346\n\n#RITU25 #VLSI #Semiconductor #ElectronicsEngineering #ChipDesign #InnovationInTech #RITKottayam #HandsOnLearning",
+        "location": "RIT",
+        "url": "https://www.yepdesk.com/embed/open-register/68e54b0746e0fb0001623c32/nd3sijvfc4",
+        "contact": [
+            {
+                "name": "Rishan",
+                "number": "9539096681"
+            },
+            {
+                "name": " Vismaya",
+                "number": "6235807346"
+            }
+        ],
+        "endDate": "2025-10-19T00:00:00+05:30",
+        "createdDate": "2025-10-09T09:22:45.044Z"
+    }, {
+        "image": "/posters/workshops/navarasa.webp",
+        "date": "2025-10-18T00:00:00+05:30",
+        "title": "Navarasa",
+        "description": "Unleash your inner artist and express the essence of emotion!\nJoin us for the Navarasa Acting Workshop, where you’ll explore the nine fundamental emotions that shape performance and storytelling.\nDiscover how to bring authenticity, depth, and creativity to every role through interactive sessions and practical exercises.\n\nDate: 18 October\nVenue: Rit Kottayam\nRegistration Fee: ₹70\n\nActivity Points: Up to 25 points\n\nRegister at: ritu25.live\n\nFor Queries:\nAthulya: 9605787220\nAnujith: 7907586381\n\n#RITU25 #Navarasa #ActingWorkshop #Drama #PerformingArts #Creativity #RITKottayam #Expression #Theatre #Workshop",
+        "location": "RIT",
+        "url": "https://www.yepdesk.com/embed/open-register/68da0ae746e0fb0001339a8b/3tgjm5vkge",
+        "contact": [
+            {
+                "name": "Athulya",
+                "number": "9605787220"
+            },
+            {
+                "name": "Anujith",
+                "number": "7907586381"
+            }
+        ],
+        "endDate": "2025-10-18T00:00:00+05:30",
+        "createdDate": "2025-10-09T09:30:56.044Z"
+    }, {
+        "image": "/posters/workshops/control.webp",
+        "date": "2025-10-17T00:00:00+05:30",
+        "title": "Control System For Robotics",
+        "description": "Discover the power behind every robotic movement!\nJoin us for an exciting workshop on Control System for Robotics and learn how intelligent machines are designed, programmed, and controlled.\nExplore the fundamentals of control systems, sensors, and actuators that bring robots to life — guided by an industry expert.\n\nSpeaker:\nShoby George\nSenior Control System Engineer, Genrobotics\n\nDate: 17 October\nVenue: Rit Kottayam\nRegistration Fee: ₹80 (ISTE Members) / ₹140 (Non-ISTE Members)\n\nActivity Points: Up to 20 points\n\nRegister at: ritu25.live\n\nFor Queries:\nArchana: 8848349960\nAysha Lina: 8590611875\n\n#RITU25 #Robotics #ControlSystems #Automation #Genrobotics #Technology #Engineering #RITKottayam #HandsOnLearning",
+        "location": "RIT",
+        "url": "https://www.yepdesk.com/embed/open-register/68e77b8546e0fb00016dbcdc/6r4qn0o998",
+        "contact": [
+            {
+                "name": "Archana",
+                "number": "8848349960"
+            },
+            {
+                "name": "Aysha Lina",
+                "number": "8590611875"
+            }
+        ],
+        "endDate": "2025-10-17T00:00:00+05:30",
+        "createdDate": "2025-10-09T09:35:53.090Z"
+    }, {
+        "image": "/posters/workshops/cyber.webp",
+        "date": "2025-10-17T00:00:00+05:30",
+        "title": "Cybersecurity 101",
+        "description": "Step into the frontlines of digital defense! \nJoin us for Cybersecurity 101, a workshop that uncovers the essentials of ethical hacking, data protection, and system security.\nLearn how to safeguard networks, identify vulnerabilities, and understand the mindset of a hacker — guided by leading experts in the field.\n\nSpeakers:\nAbhikrishna – Security Architect, Cybernix\nShadirvan MC – Senior Penetration Tester, Cybernix\n\nDate: 17 October\nVenue: Rit Kottayam\nRegistration Fee: ₹399\n\nCertificates will be provided\nActivity Points: Up to 50 points\n\nRegister at: ritu25.live\n\nFor Queries:\nRoshny: 8921132077\nEzekiel: 8301866113\n\n#RITU25 #Cybersecurity #EthicalHacking #DataProtection #TechWorkshop #RITKottayam #DigitalDefense #HandsOnLearning",
+        "location": "RIT",
+        "url": "https://www.yepdesk.com/embed/open-register/68e77e3e46e0fb00016dcfc2/e7aos5ssvh",
+        "contact": [
+            {
+                "name": "Roshny ",
+                "number": "8921132077"
+            },
+            {
+                "name": "Ezekiel",
+                "number": "8301866113"
+            }
+        ],
+        "endDate": "2025-10-17T00:00:00+05:30",
+        "club": "IEDC",
+        "createdDate": "2025-10-09T09:42:23.999Z"
     }
 ];
 
