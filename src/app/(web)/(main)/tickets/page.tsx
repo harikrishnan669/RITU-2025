29c7be910fc9d768817308546148302d7c5871a7
fbn776 <fbn776@gmail.com>
2025-10-10 15:53:40 +0530

    add new poster

--- src/app/(web)/(main)/tickets/page.tsx
+++ src/app/(web)/(main)/tickets/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/header";
-import Link from "next/link";
 import GradientLine from "@/components/gradient-line";
 import React from "react";
 
@@ -13,26 +12,26 @@ export default function TicketPage() {
         </p>
 
         <div className="px-20 gap-30 overflow-hidden w-full flex flex-row max-sm:flex-col justify-around max-sm:justify-center items-center max-md:gap-10 py-2 max-md:py-0 max-md:pt-2">
-            <Link href="/workshop"
+            <a href="#"
                   className="flex-1 transition-transform duration-100 cursor-pointer select-none hover:scale-105"
             >
-                <img alt="" src="/day-pass/day1.webp"
+                <img alt="Day 1 Pass" src="/day-pass/day1.webp"
                      className="max-md:w-[65vw]"/>
-            </Link>
-            <Link href="/workshop"
+            </a>
+            <a href="https://www.yepdesk.com/embed/buy-tickets/68d3b8d346e0fb00011f7e64/private/4f5h1mqrgl"
                   className="flex-1 transition-transform duration-100 cursor-pointer select-none hover:scale-105"
             >
-                <img alt="" src="/day-pass/day2.webp"
+                <img alt="Day 2 Pass" src="/day-pass/day2.webp"
                      className="max-md:w-[65vw]"/>
-            </Link>
+            </a>
 
-            <Link href="/workshop"
+            <a href="https://www.yepdesk.com/embed/buy-tickets/68e8046746e0fb000170463c/private/l5umhjmd3p"
                   className="flex-1 transition-transform duration-100 cursor-pointer select-none hover:scale-105"
 
             >
-                <img alt="" src="/day-pass/day3.webp"
+                <img alt="Day 3 Pass" src="/day-pass/day3.webp"
                      className="max-md:w-[65vw]"/>
-            </Link>
+            </a>
 
             <GradientLine/>
         </div>
--- src/data/proshows.ts
+++ src/data/proshows.ts
@@ -11,17 +11,19 @@ const PROSHOWS: IProshows[] = [
         img: "/posters/proshows/laqshay.webp",
         placeholder: true,
         highlight: true,
-        href: "https://www.yepdesk.com/embed/buy-tickets/68e66bda46e0fb000166b829/private/7klucegivq"
+        pageLink: "/tickets"
+        // href: "https://www.yepdesk.com/embed/buy-tickets/68e66bda46e0fb000166b829/private/7klucegivq"
     },
-    {
-        img: "/posters/proshows/day-pass.webp",
-        placeholder: true,
-        href: "https://www.yepdesk.com/embed/buy-tickets/68e66bda46e0fb000166b829/private/7klucegivq"
-    },
+    // {
+    //     img: "/posters/proshows/day-pass.webp",
+    //     placeholder: true,
+    //     href: "https://www.yepdesk.com/embed/buy-tickets/68e66bda46e0fb000166b829/private/7klucegivq"
+    // },
     {
         img: "/posters/proshows/jobkurian.webp",
         placeholder: true,
-        href: "https://www.yepdesk.com/embed/buy-tickets/68e66bda46e0fb000166b829/private/7klucegivq"
+        pageLink: "/tickets"
+        // href: "https://www.yepdesk.com/embed/buy-tickets/68e66bda46e0fb000166b829/private/7klucegivq"
     }, {
         img: "/posters/events/job-fair.webp",
         placeholder: true,
