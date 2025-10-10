1e370f3ee70a196e6b0508260709a0cc05e25e11
fbn776 <fbn776@gmail.com>
2025-10-10 21:11:41 +0530

    update

--- src/data/events/gaming.ts
+++ src/data/events/gaming.ts
@@ -3,7 +3,7 @@ import {IEventData} from "@/types/event";
 const GAMING: IEventData[] = [
     {
         "image": "/posters/gaming/BGMI.webp",
-        "date": "2025-10-11T00:00:00+05:30",
+        "date": "2025-10-13T16:00:00+05:30",
         "title": "BGMI TOURNAMENT",
         description: `Get ready to drop in and dominate the battlefield!
 Join the BGMI Tournament at RITU 25 and compete for glory, strategy, and prizes worth 10K. Assemble your dream squad and take the fight to the next level in this thrilling esports event — plus, get the exclusive chance to meet and greet the one and only Sayip OP!
@@ -18,7 +18,7 @@ Register at: ritu25.live
 #RITU25 #BGMI #SayipOP #GamingTournament #Esports #BattleRoyale #RITKottayam #TechFest #SquadUp`,
         "location": "Online",
         "url": "https://www.yepdesk.com/embed/open-register/68e68bc746e0fb0001674194/1bceh8cdqc",
-        "endDate": "2025-10-11T00:00:00+05:30",
+        "endDate": "2025-10-13T16:00:00+05:30",
         "badge": "competition",
         "createdDate": "2025-10-08T16:51:24.283Z",
         contact: [
