6cf279f49f7927a72f7c5a7089507b0d5b14772f
fbn776 <fbn776@gmail.com>
2025-10-12 16:25:40 +0530

    update

--- src/data/dept/mca.ts
+++ src/data/dept/mca.ts
@@ -12,11 +12,11 @@ photos:
 
 const MCA_DEPT: IEventData[] = [
     {
-        "image": "",
+        "image": "/posters/mca/human-processor.webp",
         "date": "10/18/2025",
         "title": "Human Processor : Data Race",
         "description": "The Human Processor: Data Race is a fast-paced team challenge testing memory, logic, and coordination through a thrilling three-stage relay.",
-        "url": "Null ",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab87346e0fb0001788dd8/tib9rev99t",
         "contact": [
             {
                 "name": "Vishnu",
@@ -28,11 +28,11 @@ const MCA_DEPT: IEventData[] = [
         "createdDate": "10/12/2025 13:17:45"
     },
     {
-        "image": "",
+        "image": "/posters/mca/tech-tri.webp",
         "date": "10/18/2025",
         "title": "Tech Triathlon ",
         "description": "A three-stage technical challenge combining quiz, coding, and debugging skills.",
-        "url": "Null",
+        "url": "https://www.yepdesk.com/embed/open-register/68eaba1146e0fb000178924e/g8ilrqu2um",
         "contact": [
             {
                 "name": "Jenin",
@@ -48,11 +48,10 @@ const MCA_DEPT: IEventData[] = [
         "createdDate": "10/12/2025 13:15:26"
     },
     {
-        "image": "",
+        "image": "/posters/mca/web-dev.webp",
         "date": "10/18/2025",
         "title": "Web Development Workshop ",
         "description": "Hands-on workshop, where students will learn by building real web pages ",
-        "url": "Null",
         "contact": [
             {
                 "name": "Rinu Fathima",
@@ -64,11 +63,10 @@ const MCA_DEPT: IEventData[] = [
         "createdDate": "10/12/2025 13:12:14"
     },
     {
-        "image": "",
+        "image": "/posters/mca/playstation.webp",
         "date": "10/17/2025",
         "title": "Play Station Gaming Arena",
         "description": "A dedicated area where participants can play and experience games on the PS5 console, offering fun and entertainment for all visitors.",
-        "url": "-",
         "contact": [
             {
                 "name": "Harinarayanan G",
@@ -80,15 +78,15 @@ const MCA_DEPT: IEventData[] = [
         "createdDate": "10/12/2025 13:10:56"
     },
     {
-        "image": "",
+        "image": "/posters/mca/efootball.webp",
         "date": "10/17/2025",
         "title": "E Football ",
         "description": "A competitive E-Sports showdown where participants go head-to-head in PUBG and eFootball, testing their strategy, teamwork, and gaming skills to emerge as champions.",
-        "url": "-",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab72346e0fb00017887f7/4uohm2tv2g",
         "contact": [
             {
                 "name": "Jerin Sebastian",
-                "number": ""
+                "number": "95671 73053"
             }
         ],
         "location": "RIT",
@@ -96,20 +94,40 @@ const MCA_DEPT: IEventData[] = [
         "createdDate": "10/12/2025 13:09:18"
     },
     {
-        "image": "",
+        "image": "/posters/mca/bgmi.webp",
         "date": "10/17/2025",
         "title": "BGMI",
         "description": "A competitive E-Sports showdown where participants go head-to-head in PUBG and eFootball, testing their strategy, teamwork, and gaming skills to emerge as champions.",
-        "url": "Null",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab7b346e0fb0001788a50/quktat473a",
         "contact": [
             {
                 "name": "Jerin Sebastian",
-                "number": ""
+                "number": "9567173053"
             }
         ],
         "location": "RIT ",
         "endDate": "10/17/2025",
         "createdDate": "10/12/2025 13:07:04"
+    },
+    {
+        "image": "/posters/mca/takedown.webp",
+        "date": "10/17/2025",
+        "title": "Takedown: Rogue AI ",
+        "description": "Takedown:Rogue AI is a team challenge where participants solve puzzles, decrypt codes, and work together to “shut down” a rogue AI before time runs out.",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab6f346e0fb0001788779/btgtq7gehg",
+        "contact": [
+            {
+                "name": "Jishnu PM",
+                "number": "8086636523"
+            },
+            {
+                "name": "Fathima Nishva ",
+                "number": "8590469564"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/17/2025",
+        "createdDate": "10/12/2025 16:14:59"
     }
 ];
 
