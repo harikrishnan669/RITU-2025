1455419c25e9444da4fd30f379581308bfa8f8ea
fbn776 <fbn776@gmail.com>
2025-10-12 17:34:44 +0530

    update

--- src/data/socities-mapping.ts
+++ src/data/socities-mapping.ts
@@ -4,11 +4,9 @@ const SOCIETIES_MAPPING = [
     {
         image: "/society/ieee.webp",
         page: "/society/ieee",
-        comingSoon: true
     }, {
         image: "/society/iedc.webp",
         page: "/society/iedc",
-        comingSoon: true
     }, {
         image: "/society/tinkerhub.webp",
         page: "/society/tinkerhub",
