044c25c8b81bbefc93e81e2c907c50e81d70cdfc
fbn776 <fbn776@gmail.com>
2025-10-12 15:34:58 +0530

    update

--- src/data/dept-mapping.ts
+++ src/data/dept-mapping.ts
@@ -2,7 +2,7 @@ const DEPT_MAPPING = [
     {image: "/dept/arch.webp", page: "/dept/arch", comingSoon: true},
     {image: "/dept/civil.webp", page: "/dept/civil"},
     {image: "/dept/cse.webp", page: "/dept/cse"},
-    {image: "/dept/ece.webp", page: "/dept/ece", comingSoon: true},
+    {image: "/dept/ece.webp", page: "/dept/ece"},
     {image: "/dept/eee.webp", page: "/dept/eee", comingSoon: true},
     {image: "/dept/mca.webp", page: "/dept/mca", comingSoon: true},
     {image: "/dept/mech.webp", page: "/dept/mech", comingSoon: true},
--- src/data/dept/ece.ts
+++ src/data/dept/ece.ts
@@ -1,6 +1,249 @@
 import {IEventData} from "@/types/event";
+/*
+click.webp
+drone-expo.webp
+f1.webp
+neon-room.webp
+photoshop.webp
+project-expo.webp
+scrap-to-bot.webp
+semiconductor.webp
+verilog.webp
+wave-form.webp
+ */
 
-
-const ECE_DEPT: IEventData[] = [];
+const ECE_DEPT: IEventData[] = [
+    {
+        "image": "/posters/ece/photoshop.webp",
+        "date": "10/17/2025",
+        "title": "PHOTOSHOP WORKSHOP",
+        "description": "\"🪄 Transform Your Imagination into Digital Art!\n\n🧠 Dive into the world of visual creativity and design at the Photoshop Workshop, hosted by the ECE Department, RIT Kottayam — where art meets innovation!\n\n🎨 Sketch. Edit. Inspire.\nStep beyond filters and tools — learn to design captivating posters, enhance photos like a pro, and craft your own digital masterpiece. Whether you’re a beginner or a creator at heart, this workshop will help you bring ideas to life.\n\n🌟 What Awaits You:\n🖱️ Guided sessions on Adobe Photoshop essentials\n🧩 Hands-on creative tasks and mini challenges\n🖼️ Craft your own artwork by the end of the workshop\n🏆 Top designs will be featured on RITU’25 official pages!\n\n📅 Date: 17 October\n📍 Venue: ECE Department, RIT Kottayam\n💰 Registration Fee: ₹30 only\n🎖️ Activity Points: Up to 30 points\n\n🔗 Register Now: ritu25.live\n\n📞 For Queries:\nSriram – 8075629282\n\n#RITU25 #PhotoshopWorkshop #DesignMagic #DigitalCreativity #InnovationThroughArt #GraphicDesign #ECEDept #RITKottayam #VisualDesign #HandsOnWorkshop\"",
+        "url": "https://www.yepdesk.com/embed/open-register/68eaaf9146e0fb00017870b2/kgp6ol2279",
+        "contact": [
+            {
+                "name": "Sriram",
+                "number": "8075629282"
+            },
+            {
+                "name": "Samekh",
+                "number": "9497679893"
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/17/2025",
+        "createdDate": "10/12/2025 12:37:35"
+    },
+    {
+        "image": "/posters/ece/verilog.webp",
+        "date": "10/19/2025",
+        "title": "VERILOG AND LCD QUIZ",
+        "description": "\"💻 Test Your Skills — Verilog & LCD Quiz!\n\n🔹 The ECE Department, RIT Kottayam presents the Verilog & LCD Quiz, a fun and interactive way to challenge your knowledge of digital design and embedded systems!\n\n⚡ Think. Solve. Display.\nShow your expertise in Verilog coding, digital logic, and LCD interfacing. A perfect quiz for tech enthusiasts who love hands-on digital circuits!\n\n📅 Date: 19 October\n📍 Venue: ECE Department, RIT Kottayam\n💰 Registration Fee: ₹30 only\n\n🔗 Register now: ritu25.live\n\n📞 For Queries:\nVaishnavi – +91 70129 99407\nJorlin – +91 85929 06952\n\n#RITU25 #VerilogLCDQuiz #DigitalDesign #ECEDept #RITKottayam #TechTrivia #EmbeddedSystems #LogicDesign\n\"",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab32346e0fb0001787d7e/let55ha3jj",
+        "contact": [
+            {
+                "name": "Vaishnavi ",
+                "number": ""
+            },
+            {
+                "name": "Jorlin",
+                "number": ""
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/19/2025",
+        "createdDate": "10/12/2025 12:33:19"
+    },
+    {
+        "image": "/posters/ece/project-expo.webp",
+        "date": "10/19/2025",
+        "title": "PROJECT EXPO",
+        "description": "\"✨🚀 Get ready to showcase the future of innovation!\n\n🌟 Step into the world of creativity & technology at our Project Expo, where brilliant ideas 💡 meet real-world solutions 🌍.\n\n🤖 From Electronics & Robotics ⚡ to Software Development 💻 and Sustainable Tech 🌱, this expo is the perfect platform to:\n🔹 Present your innovative projects 🎯\n🔹 📘 Exchange knowledge\n🔹 🌟 Inspire and be inspired\n\n📆 Date: 19-10-25\n📍 Venue: ECE Dept.\n💸 Reg fees: ₹100\n🏆 Prize pool: ₹5,000\n\n🛑 Guidelines:\n\n1. Projects should be based on the ECE domain, including areas such as IoT, Embedded Systems, and Smart Electronics.\n2.Participants should clearly explain the objective,  principle, and applications of their project.\n3.Each team can have a maximum of 5 members.\n\n\n\n🔗 Register Now: 📌 ritu25.live\n\n📞 For Queries: Grace – 9995822617\n\n💬 Stay Updated — Follow the Official RITU’25 Channel:\n📲 [WhatsApp Channel Link]\n\n#RITU25 #ProjectExpo #InnovationInAction #TechFestVibes #Electronics #Robotics #SoftwareDevelopment #SustainableTech #FutureOfInnovation #ECEProjects #CreativeTech #InspireAndInnovate\"",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab2be46e0fb0001787b9a/kddt943tha",
+        "contact": [
+            {
+                "name": "Grace",
+                "number": "9995822617"
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/19/2025",
+        "createdDate": "10/12/2025 12:31:27"
+    },
+    {
+        "image": "/posters/ece/neon-room.webp",
+        "date": "10/19/2025",
+        "title": "NEON ROOM",
+        "description": "\"🌟 Step Into a World of Glow and Vibes!\n\n🎨 Experience the Magic of the Neon Room — Light Up Your Creativity!\nThe ECE Department invites you to an electrifying Neon Room Experience at RIT Kottayam — where colors, lights, and music blend to create an unforgettable atmosphere!\n\n✨ Glow. Chill. Click.\nDive into a visually stunning neon setup, perfect for photography, socializing, and enjoying the ultimate immersive vibe. Let your creativity shine under vibrant lights and capture memories that glow!\n\n🎯 Highlights:\n💡 Neon-Themed Interactive Zones\n📸 Photo Opportunities & Creative Corners\n🎶 Music & Chill Vibes\n\n📅 Date: 19 October 2025\n📍 Venue: 4th Year Classroom, ECE Department, RIT Kottayam\n💸 Entry Fee: Free for all RITU’25 participants\n\n\n🔗 Register now: ritu25.live\n\n📞 For Queries:\nAbhinav – +91 90724 05062\"",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab0ed46e0fb0001787799/tjp1g4kmjp",
+        "contact": [
+            {
+                "name": "Abhinav ",
+                "number": ""
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/19/2025",
+        "createdDate": "10/12/2025 12:29:30"
+    },
+    {
+        "image": "/posters/ece/scrap-to-bot.webp",
+        "date": "10/18/2025",
+        "title": "SCRAP TO BOT",
+        "description": "\"♻ Turn Trash into Tech! 🤖\nGet ready to innovate at Scrap to Bot, a thrilling technical competition where creativity meets sustainability! 💡\n\nIn this challenge, participants will design and build functional robots using scrap and discarded materials. Apply your knowledge of mechanical design, electronics, and control systems to transform waste into working wonders. From brainstorming concepts to building prototypes, this event tests your innovation, problem-solving, and engineering skills.\n\nWhether you’re a budding engineer or a robotics enthusiast, Scrap to Bot is your chance to prove that great technology can emerge from unexpected places!\n\n📅 Date: 17 October\n📍 Venue: ECE Department, RIT Kottayam\n💰 Registration Fee: ₹30\n\n🔗 Register now at: ritu25.live\n\n📞 Event Coordinators:\nSivananda – 8848945721\nSuryakiran – 9599443230\n\n#RITU25 #ScrapToBot #TechForChange #InnovationInAction #Sustainability #Robotics #HandsOnLearning #ElectronicsEngineering #RITKottayam\"",
+        "url": "https://www.yepdesk.com/embed/open-register/68eaaed846e0fb0001786edd/5ktu9c5rd",
+        "contact": [
+            {
+                "name": "Sivananda",
+                "number": "8848945721"
+            },
+            {
+                "name": "Suryakiran",
+                "number": "9599443230"
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/18/2025",
+        "createdDate": "10/12/2025 12:19:15"
+    },
+    {
+        "image": "/posters/ece/semiconductor.webp",
+        "date": "10/17/2025",
+        "title": "SEMICONDUCTOR QUIZ",
+        "description": "\"🔋 Think Fast, Think Smart — It’s Time for the Semiconductor Quiz!\n\n💡 The ECE Department, RIT Kottayam presents the Semiconductor Quiz, a fun and engaging challenge to test your knowledge of electronics, circuits, and the fascinating world of semiconductors!\n\n⚙ Answer. Compete. Learn.\nWhether you’re a quiz enthusiast or an electronics lover, this is your chance to showcase your concepts, learn new facts, and enjoy a friendly battle of brains.\n\n📅 Date: 17 October\n📍 Venue: ECE Department, RIT Kottayam\n💰 Registration Fee: ₹30 only\n\n🔗 Register now: ritu25.live\n\n📞 For Queries:\nAbhijith – +91 97782 97013\nAnaika – +91 73067 05704\n\n#RITU25 #SemiconductorQuiz #ECEDept #RITKottayam #TechTrivia #ElectronicsQuiz #KnowledgeChallenge #LearnAndCompete\"",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab05546e0fb0001787459/h68d6lvia6",
+        "contact": [
+            {
+                "name": "Abhijith",
+                "number": "9778297013"
+            },
+            {
+                "name": "Anaika",
+                "number": ""
+            }
+        ],
+        "location": "ECE DEPARTMENT ",
+        "endDate": "10/17/2025",
+        "createdDate": "10/12/2025 12:16:57"
+    },
+    {
+        "image": "/posters/ece/wave-form.webp",
+        "date": "10/17/2025",
+        "title": "WAVEFORM CLONING COMPETITION",
+        "description": "⚡ Crack the Code, Chase the Current, Command the Circuit ⚡\n\nRITU’25 proudly unveils\n\n WAVECRAFT 📡\n\nWhere precision meets pulse — recreate, realign, and resonate with the rhythm of waves! ⚙\n\n🗓 Date: October 16, 2025\n🎟 Registration Fee: ₹100\n💰 Prize Pool: ₹2,000\n\n\nGet ready to test your circuit instincts, sync with the signal, and rise above the noise! ⚡\n\n📞 Contact:\nAaditya Narayana – 8590608217\nAswathy -+91 85905 77502",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab0f046e0fb00017877c8/edo4mt2rhu",
+        "contact": [
+            {
+                "name": "Aaditya Narayana ",
+                "number": "8590608217"
+            },
+            {
+                "name": "Aswathy ",
+                "number": ""
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/17/2025",
+        "createdDate": "10/12/2025 12:14:46"
+    },
+    {
+        "image": "/posters/ece/f1.webp",
+        "date": "10/17/2025",
+        "title": "F1 RC CAR RACE",
+        "description": "\"🏎 Fuel Your Fun with the F1 Racing Challenge!\n\n💫 The ECE Department, RIT Kottayam invites you to a light-hearted dose of speed and excitement with the F1 Racing Challenge — an event for everyone who loves a little friendly competition!\n\n⚙ Steer. Race. Enjoy.\nTake the driver’s seat in this mini F1 racing game and test your control, timing, and focus. No pressure — just pure fun and a chance to enjoy the thrill of racing with your friends!\n\n🎯 Event Details:\n🎮 Easy-to-play F1 simulator setup\n🏁 Compete for best lap timings\n\n📅 Dates: 17, 18 & 19 October\n📍 Venue: ECE Department, RIT Kottayam\n💰 Registration Fee: ₹50 (Spot Registration)\n\n📞 For Queries:\nAdwaith – 9447752740\nMishal – 9946677293\n\n#RITU25 #F1RacingChallenge #FunEvent #ECEDept #RITKottayam #MiniRace\"",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab00846e0fb00017873a4/uoe9nbpnr8",
+        "contact": [
+            {
+                "name": "Adwaith",
+                "number": "9447752740"
+            },
+            {
+                "name": "Mishal",
+                "number": "9946677293"
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/19/2025",
+        "createdDate": "10/12/2025 12:05:09"
+    },
+    {
+        "image": "/posters/ece/drone-expo.webp",
+        "date": "10/18/2025",
+        "title": "DRONE EXPO",
+        "description": "\"🛸 Soar Beyond Limits with Innovation!\n\n🚁 Experience the Future of Flight — Live at the Drone Expo 2025!\nTelesto, in association with the ECE Department, proudly presents an electrifying Drone Expo — a showcase where technology takes flight and imagination knows no bounds!\n\n✨ Explore. Learn. Innovate.\nWitness cutting-edge drones in action, discover the science behind flight control and automation, and interact with pioneers pushing the boundaries of aerial innovation. From scouting and delivery drones to AI-powered surveillance systems — experience it all, right here at RIT!\n\n🎯 Highlights:\n🌐 Live Drone Demonstrations\n🔍 Insights into Drone Technology & Design\n💡 Meet Innovators & Project Teams\n🎓 Learn about Real-World Applications in Disaster Management, Surveillance & More\n\n📅 Date: 18 October 2025\n📍 Venue: ECE Department, RIT Kottayam\n💸 Entry Fee: Free for all RITU’25 participants\n🏅 Activity Points: Up to 50 points\n\n🔗 Register now: ritu25.live\n\n📞 For Queries:\nAbhiram – 9744520508\n\n#RITU25 #DroneExpo #Telesto #AerialInnovation #Drones #TechnologyInFlight #RITKottayam #ECEDept #Automation #Innovation #FutureOfTech\n\n💬 Stay Updated — Follow the Official RITU’25 Channel:\n👉 WhatsApp Channel Link\n\n---\"",
+        "contact": [
+            {
+                "name": "ABHIRAM",
+                "number": "9744520508"
+            },
+            {
+                "name": "NIRANJANA",
+                "number": "8075950406"
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/18/2025",
+        "createdDate": "10/12/2025 12:02:43"
+    },
+    {
+        "image": "/posters/ece/makethon.webp",
+        "date": "10/17/2025",
+        "title": "MAKETHON X DESIGN CLUB",
+        "description": "\"💡 Innovate. Build. Transform.\nGet ready to bring your ideas to life at MAKEATHON 2025 — the ultimate innovation marathon where creativity meets technology! ⚙️✨\n\n🤝 In collaboration with the Department of ECE and the Design Club, this event brings together creativity, technical brilliance, and teamwork to turn ideas into reality.\n\n👥 Collaborate. Prototype. Compete.\nForm your dream team, brainstorm groundbreaking solutions, and design real-world prototypes that redefine the future! 🌍💫\n\n📅 Date: 17th October 2025\n🕒 Time: 9:00 AM - 4:00 PM\n📍 Venue: ECE Department, RIT Kottayam\n💸 Registration Fee: ₹150 per team\n🏆 Exciting Prizes & Certificates Await!\n🔗 Register Now: ritu25.live\n\n📞 For Queries:\nAdithya - 9946538552\nAnuja - 9074713769\n\n\n---\n\n⚙️ MAKEATHON 2025 - GUIDELINES\n\n• Teams can consist of 3 to 4 members.\n• All required components will be provided (except laptops).\n• The project idea will be revealed at the time of the event.\n• Teams can use any online platforms or references during the event.\n\n🏅 Activity Points: Up to 50\n💰 Prize Pool: ₹2,500\n\n\n---\n\n💬 Dream. Design. Deliver.\nJoin the movement that shapes tomorrow - only at MAKEATHON 2025! 🚀\n\n#RITU25 #Makeathon2025 #InnovationMarathon #TechForGood #PrototypeToProduct #CreateTheFuture #RITKottayam #ECEDept #DesignClub #EngineeringInnovation #TeamWork #IdeathonToReality\"",
+        "url": "https://www.yepdesk.com/embed/open-register/68eab3b946e0fb0001787f79/2j7892mh4s",
+        "contact": [
+            {
+                "name": "Adithya",
+                "number": "9946538552"
+            },
+            {
+                "name": "Anuja ",
+                "number": "9074713769"
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/17/2025",
+        "createdDate": "10/12/2025 12:35:15"
+    },
+    {
+        "image": "/posters/ece/matlab.webp",
+        "date": "10/18/2025",
+        "title": "MATLAB WORKSHOP",
+        "description": "Code. Simulate. Innovate!\nDive into the world of computation and engineering design with our MATLAB Workshop — your gateway to mastering one of the most powerful tools for engineers and researchers.\n\nGain hands-on experience in data analysis, signal processing, simulation, and visualization using MATLAB. Learn how to transform concepts into real-time results through interactive sessions and practical exercises.\n\nSpeakers: STEM EXPERT\n\n\nDate: 18 October 2025\nVenue: RIT Kottayam\nRegistration Fee: ₹150\n\nActivity Points: Up to 50 points\n\nRegister at: ritu25.live\n\nFor Queries:\nPranav:85901 86849\nSwathi:9847212106\n\n#RITU25 #MATLAB #EngineeringTools #DataAnalysis #Simulation #Coding #Innovation #Technology #RITKottayam #HandsOnLearning",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb754a46e0fb00017e85ec/st5126ikhm",
+        "contact": [
+            {
+                "name": "Pranav",
+                "number": ""
+            },
+            {
+                "name": "Swathi",
+                "number": "9847212106"
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/18/2025",
+        "createdDate": "10/12/2025 13:01:33"
+    },
+    {
+        "image": "/posters/ece/iot.webp",
+        "date": "10/18/2025",
+        "title": "IOT WORKSHOP",
+        "description": "Connect the World with the Power of IoT!\nStep into the future of smart technology with our Internet of Things (IoT) Workshop — a hands-on session where innovation meets connectivity.\nLearn how everyday devices communicate, collect data, and create intelligent systems that shape modern living. Gain practical skills in sensor integration, wireless communication, and cloud-based control.\n\nSpeakers: STEM EXPERT\n\n\nDate: 18 October 2025\nVenue: RIT Kottayam\nRegistration Fee: ₹150\n\nActivity Points: Up to 50 points\n\nRegister at: ritu25.live\n\nFor Queries:\nSreelakshmi: 9526173315\nKrishnapriya: 8907430337\n\n#RITU25 #IoT #SmartTechnology #Innovation #ElectronicsEngineering #Automation #RITKottayam #HandsOnLearning",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb766446e0fb00017e8ef8/ipl5lpjp7o",
+        "contact": [
+            {
+                "name": "Sreelakshmi",
+                "number": "9526173315"
+            },
+            {
+                "name": "Krishnapriya",
+                "number": "8907430337"
+            }
+        ],
+        "location": "ECE DEPARTMENT",
+        "endDate": "10/18/2025",
+        "createdDate": "10/12/2025 12:59:29"
+    }
+];
 
 export default ECE_DEPT;
