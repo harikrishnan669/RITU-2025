82015826a66ab311bfee83c6d3670ce9d90da5cb
fbn776 <fbn776@gmail.com>
2025-10-12 16:26:30 +0530

    update

--- src/data/dept-mapping.ts
+++ src/data/dept-mapping.ts
@@ -4,7 +4,7 @@ const DEPT_MAPPING = [
     {image: "/dept/cse.webp", page: "/dept/cse"},
     {image: "/dept/ece.webp", page: "/dept/ece"},
     {image: "/dept/eee.webp", page: "/dept/eee"},
-    {image: "/dept/mca.webp", page: "/dept/mca", comingSoon: true},
+    {image: "/dept/mca.webp", page: "/dept/mca"},
     {image: "/dept/mech.webp", page: "/dept/mech", comingSoon: true},
     {image: "/dept/mtech.webp", page: "/dept/mtech", comingSoon: true},
 ];
