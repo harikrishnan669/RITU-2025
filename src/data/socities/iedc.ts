b9b57ad987990708423daf509ed97cbfbc26c974
fbn776 <fbn776@gmail.com>
2025-10-12 17:33:09 +0530

    update

--- src/data/socities/iedc.ts
+++ src/data/socities/iedc.ts
@@ -1,8 +1,188 @@
 import {IEventData} from "@/types/event";
 
-
+/*
+/posters/iedc/business-idea.webp
+/posters/iedc/design-leap.webp
+/posters/iedc/ideathon.webp
+/posters/iedc/period-simulator.webp
+/posters/iedc/pitch-product.webp
+/posters/iedc/startup-bootcamp.webp
+/posters/iedc/startup-conclave.webp
+/posters/iedc/treasure-quest.webp
+ */
 const IEDC_DATA: IEventData[] = [
-
+    {
+        "image": "/posters/iedc/design-leap.webp",
+        "date": "10/17/2025",
+        "title": "DESIGNAI3.0",
+        "description": "Hands on workshop based on Adobe photoshop ",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb585346e0fb00017ccc1a/f08lpfb6c4",
+        "contact": [
+            {
+                "name": "Devang",
+                "number": "8590369251"
+            },
+            {
+                "name": "Gangothri",
+                "number": "8078116828"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/17/2025",
+        "createdDate": "10/12/2025 13:12:03"
+    },
+    {
+        "image": "/posters/iedc/startup-conclave.webp",
+        "date": "10/17/2025",
+        "title": "Startup conclave",
+        "description": "Meeting successful startups and entrepreneurs under one roof ",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb599f46e0fb00017cdec4/rj04fnji4o",
+        "contact": [
+            {
+                "name": "Gangothri",
+                "number": "8078116828"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/19/2025",
+        "createdDate": "10/12/2025 13:09:13"
+    },
+    {
+        "image": "/posters/iedc/period-simulator.webp",
+        "date": "10/17/2025",
+        "title": "Period pain simulator ",
+        "description": "Period Pain Simulator is a wearable device that mimics menstrual cramps using controlled electrical impulses. Designed to raise awareness and build empathy, it allows non-menstruating individuals to experience the intensity of period pain firsthand.",
+        "url": "null",
+        "contact": [
+            {
+                "name": "Akshaya",
+                "number": "6238795752"
+            },
+            {
+                "name": "Gangothri",
+                "number": "8078116828"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/19/2025",
+        "createdDate": "10/12/2025 13:03:16"
+    },
+    {
+        "image": "/posters/iedc/business-idea.webp",
+        "date": "10/19/2025",
+        "title": "Business idea pitching",
+        "description": "Present your innovative business idea that solves a real problem and has the potential to create impact.",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb519146e0fb00017c711b/tg59vvr2p2",
+        "contact": [
+            {
+                "name": "Adithya",
+                "number": "7907716324"
+            },
+            {
+                "name": "Gangothri",
+                "number": "8078116828"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/19/2025",
+        "createdDate": "10/12/2025 12:59:26"
+    },
+    {
+        "image": "/posters/iedc/startup-bootcamp.webp",
+        "date": "10/18/2025",
+        "title": "Start up bootcamp ",
+        "description": "Talk session on turning ideas into impact ",
+        "url": "null",
+        "contact": [
+            {
+                "name": "Krishnapriya S ",
+                "number": "9497786670"
+            },
+            {
+                "name": "Gangothri",
+                "number": "8078116828"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/18/2025",
+        "createdDate": "10/12/2025 12:53:27"
+    },
+    {
+        "image": "/posters/iedc/treasure-quest.webp",
+        "date": "10/17/2025",
+        "title": "Treasure quest",
+        "description": "Solving digital puzzles with a real world race to uncover hidden secrets across the campus ",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb5a3846e0fb00017cec68/2jglhcv15q",
+        "contact": [
+            {
+                "name": "Gangothri",
+                "number": "8078116828"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/18/2025",
+        "createdDate": "10/12/2025 12:51:18"
+    },
+    {
+        "image": "/posters/iedc/pitch-product.webp",
+        "date": "10/18/2025",
+        "title": "PITCH THE PRODUCT ",
+        "description": "Present your product, convince the judges of its value, and show how it can create real-world impact.",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb52f646e0fb00017c8732/af28jsmde3",
+        "contact": [
+            {
+                "name": "Gourikrishna",
+                "number": "8848188140"
+            },
+            {
+                "name": "Abhay",
+                "number": "8078732257"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/18/2025",
+        "createdDate": "10/12/2025 12:44:31"
+    },
+    {
+        "image": "/posters/iedc/ideathon.webp",
+        "date": "10/18/2025",
+        "title": "IDEATHON",
+        "description": "Developing innovative solutions for real world problems as a team and discuss several aspects such as technical challenges, monetization etc",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb525846e0fb00017c7ca6/plj3mngeic",
+        "contact": [
+            {
+                "name": "Sivakrishna ",
+                "number": "9778218997"
+            },
+            // {
+            //     "name": "Sreenandana",
+            //     "number": ""
+            // }
+        ],
+        "location": "RIT",
+        "endDate": "10/18/2025",
+        "createdDate": "10/12/2025 12:38:17"
+    },
+    {
+        "image": "/posters/iedc/financial.webp",
+        "date": "10/17/2025",
+        "title": "Financial literacy and trading",
+        "description": "Talk session on financial literacy ",
+        "url": "https://www.yepdesk.com/embed/open-register/68eb57a846e0fb00017cc21f/23mc9da8hu",
+        "contact": [
+            {
+                "name": "Abhijith",
+                "number": ""
+            },
+            {
+                "name": "Gangothri",
+                "number": "8078116828"
+            }
+        ],
+        "location": "RIT",
+        "endDate": "10/17/2025",
+        "createdDate": "10/12/2025 12:55:51"
+    }
 ]
 
 export default IEDC_DATA;
