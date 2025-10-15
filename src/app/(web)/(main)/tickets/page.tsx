5cfa76de8e5b56d4a4fa6c6da8566e9c861066aa
fbn776 <fbn776@gmail.com>
2025-10-15 19:51:54 +0530

    update

--- src/app/(web)/(main)/tickets/page.tsx
+++ src/app/(web)/(main)/tickets/page.tsx
@@ -12,30 +12,20 @@ export default function TicketPage() {
         </p>
 
         <div className="px-20 gap-30 overflow-hidden w-full flex flex-row max-sm:flex-col justify-around max-sm:justify-center items-center max-md:gap-10 py-2 max-md:py-0 max-md:pt-2">
-            <a
-                href="https://www.yepdesk.com/embed/buy-tickets/68ef678b46e0fb0001910834/private/42d851k5kd" // Flash Sale
-                // href="https://www.yepdesk.com/embed/buy-tickets/68ef439346e0fb0001904f81/private/8emivlhdsv"
-
+            <a href="https://www.yepdesk.com/embed/buy-tickets/68ef439346e0fb0001904f81/private/8emivlhdsv"
                   className="flex-1 transition-transform duration-100 cursor-pointer select-none hover:scale-105"
             >
                 <img alt="Day 1 Pass" src="/day-pass/day1.webp"
                      className="max-md:w-[65vw]"/>
             </a>
-            <a
-                href="https://www.yepdesk.com/embed/buy-tickets/68ef678b46e0fb0001910834/private/42d851k5kd" // Flash Sale
-
-                // href="https://www.yepdesk.com/embed/buy-tickets/68d3b8d346e0fb00011f7e64/private/4f5h1mqrgl"
-
+            <a href="https://www.yepdesk.com/embed/buy-tickets/68d3b8d346e0fb00011f7e64/private/4f5h1mqrgl"
                   className="flex-1 transition-transform duration-100 cursor-pointer select-none hover:scale-105"
             >
                 <img alt="Day 2 Pass" src="/day-pass/day2.webp"
                      className="max-md:w-[65vw]"/>
             </a>
 
-            <a
-                href="https://www.yepdesk.com/embed/buy-tickets/68ef678b46e0fb0001910834/private/42d851k5kd" // Flash Sale
-
-                // href="https://www.yepdesk.com/embed/buy-tickets/68e8046746e0fb000170463c/private/l5umhjmd3p"
+            <a href="https://www.yepdesk.com/embed/buy-tickets/68e8046746e0fb000170463c/private/l5umhjmd3p"
                   className="flex-1 transition-transform duration-100 cursor-pointer select-none hover:scale-105"
 
             >
