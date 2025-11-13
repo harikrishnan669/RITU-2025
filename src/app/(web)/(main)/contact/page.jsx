c8ba5aed3b58b0b6c7b658a3b347eebf67e479a2
fbn776 <fbn776@gmail.com>
2025-11-13 19:01:21 +0530

    feat: removed student coordinators names

--- src/app/(web)/(main)/contact/page.jsx
+++ src/app/(web)/(main)/contact/page.jsx
@@ -1,8 +1,8 @@
 "use client";
-import ContactCard from "@/components/contact/contact-card";
+// import ContactCard from "@/components/contact/contact-card";
 import WebCard from "@/components/contact/web-card";
 import LandingBanner from "@/components/pages/landing-banner";
-import COORDINATORS from "@/data/coordinators";
+// import COORDINATORS from "@/data/coordinators";
 import WEB_TEAM from "@/data/web-team";
 
 export default function TEst() {
@@ -15,24 +15,24 @@ export default function TEst() {
                 title="Contact"
                 description="We’d love to hear from you! Reach out to our team for any queries, details, or support, and stay connected with the fest every step of the way."
             />
-            <div className="relative">
-                <div className="relative z-20 flex flex-col items-center justify-center  px-8 py-16">
-                    <h2 className="text-4xl mb-8 max-md:mb-4  text-white text-center tracking-widest max-md:text-xl font-bold">
-                        STUDENT COORDINATORS
-                    </h2>
-                    <div className="w-full my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-[1fr_1fr_1fr]  gap-6 md:gap-12">
-                        {COORDINATORS.map((item, i) =>
-                            <ContactCard
-                                key={i}
-                                imgSource={item.image || '/contact/doc1.png'}
-                                name={item.name || 'Coordinator'}
-                                position={item.position || 'coordinator'}
-                                contact={item.phone || '1234567890'}
-                            />
-                        )}
-                    </div>
-                </div>
-            </div>
+            {/*<div className="relative">*/}
+            {/*    <div className="relative z-20 flex flex-col items-center justify-center  px-8 py-16">*/}
+            {/*        <h2 className="text-4xl mb-8 max-md:mb-4  text-white text-center tracking-widest max-md:text-xl font-bold">*/}
+            {/*            STUDENT COORDINATORS*/}
+            {/*        </h2>*/}
+            {/*        <div className="w-full my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-[1fr_1fr_1fr]  gap-6 md:gap-12">*/}
+            {/*            {COORDINATORS.map((item, i) =>*/}
+            {/*                <ContactCard*/}
+            {/*                    key={i}*/}
+            {/*                    imgSource={item.image || '/contact/doc1.png'}*/}
+            {/*                    name={item.name || 'Coordinator'}*/}
+            {/*                    position={item.position || 'coordinator'}*/}
+            {/*                    contact={item.phone || '1234567890'}*/}
+            {/*                />*/}
+            {/*            )}*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
             <div>
                 <div className="relative z-20 flex flex-col items-center justify-center  px-8">
                     <h2 className="text-4xl mb-8 max-md:mb-4  text-white text-center tracking-widest max-md:text-xl font-bold">
